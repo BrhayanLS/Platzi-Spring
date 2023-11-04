fff8f2f9fd6a69baa7a2c177771cb9f1ab096fb9
BrhayanLS <brhayanls11@gmail.com>
2023-11-03 23:34:19 -0500

    Hemos finalizado el curso y la APi construida con Spring esta funcional, aunque por cambios en Heroku no se pudo desplegar

--- src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
@@ -1,2 +1,27 @@
-package com.platzi.market.persistence.mapper;public interface PurchaseItemMapper {
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.PurchaseItem;
+import com.platzi.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            //@Mapping(source = "total", target = "total"),No se añade porque el nombre en ambos lados es igual
+            @Mapping(source = "estado", target = "active")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true),
+    })
+    ComprasProducto toComprasProducto(PurchaseItem item);
 }
